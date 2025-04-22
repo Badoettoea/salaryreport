75447d599d36a9043acde483c2cace9d379ad82d
Badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-04-22 10:27:45 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.text());
+
+// Serve static files
+app.use(express.static('.'));
+
+// Update announcement
+app.put('/announcement.txt', (req, res) => {
+  fs.writeFileSync('announcement.txt', req.body);
+  res.send('Announcement updated');
+});
+
+// Update data.csv
+app.put('/data.csv', (req, res) => {
+  fs.writeFileSync('data.csv', req.body);
+  res.send('Data updated');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
